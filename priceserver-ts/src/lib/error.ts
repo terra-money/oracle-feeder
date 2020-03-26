da83e1a5b078d37ffe20fd501bc264322a7bf8f6
Alex Kim <alex@terra.money>
2020-03-26 15:05:52 +0900

    fix: skip specific errors without reporting

--- priceserver-ts/src/lib/error.ts
+++ priceserver-ts/src/lib/error.ts
@@ -2,6 +2,10 @@ import * as sentry from '@sentry/node';
 import * as logger from './logger';
 
 export function errorHandler(error: any) {
+  if (typeof error === 'string' && error === 'skip') {
+    return;
+  }
+
   logger.error(error);
   sentry.captureException(error);
 }
