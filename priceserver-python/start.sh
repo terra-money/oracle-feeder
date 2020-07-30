4487eb07b9828fe63bd662cec141c903753d637e
Paul Kim <paul@terra.money>
2020-07-30 14:19:04 +0900

    Removed priceserver-python

--- priceserver-python/start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export PYTHONPATH=${PYTHONPATH:-.}
-export FLASK_ENV=${FLASK_ENV:-develop}
-
-case "${FLASK_ENV}" in
-    production) FLASK_APP=application.py flask run --port=7658 --eager-loading $*;;
-             *) FLASK_APP=application.py FLASK_DEBUG=1 flask run --port=5000 --eager-loading $*;;
-esac
