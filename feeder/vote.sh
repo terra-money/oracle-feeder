24f28231a706d13705e06a354326aa15ce1d3390
yys <sw.yunsuk@gmail.com>
2020-09-04 17:25:54 +0900

    remove broadcast success log check; tx does not contain success field now

---
+++ feeder/vote.sh
@@ -0,0 +1,7 @@
+npm start vote -- \
+--source http://localhost:8532/latest \
+--lcd http://localhost:1317 \
+--chain-id testnet \
+--denoms sdr,krw,usd,mnt \
+--validator terravaloper1nty4ku4a79zj45jl0fy5rrjun07ny0nrvkj04k \
+--password 12345678
