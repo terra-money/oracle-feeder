be11d830433ab646269ee44e0a99e51e059bc844
Paul Kim <paul@terra.money>
2020-12-19 23:37:05 +0900

    feat(provider): GateIO

---
+++ price-server/src/provider/crypto/quoter/GateIO.ts
@@ -0,0 +1,55 @@
+import * as GateAPI from 'gate-api'
+import { num } from 'lib/num'
+import { errorHandler } from 'lib/error'
+import { Quoter, Trades } from 'provider/base'
+
+const apiInstance = new GateAPI.SpotApi()
+
+export class GateIO extends Quoter {
+  private async fetchLatestTrades(symbol: string): Promise<Trades> {
+    return new Promise((resolve, reject) => {
+      const currencyPair = symbol.replace('/', '_')
+
+      apiInstance.listCandlesticks(
+        currencyPair,
+        {
+          limit: 200,
+          interval: '1m',
+        },
+        (error, data) => {
+          if (error) {
+            reject(error)
+          } else {
+            let i = 0
+            const trades: Trades = []
+
+            while (i < data.length) {
+              // eslint-disable-next-line @typescript-eslint/no-unused-vars
+              const [timestamp, volume, close, high, low, open] = data[i++]
+              trades.push({ price: num(close), volume: num(volume), timestamp: +timestamp })
+            }
+
+            resolve(trades)
+          }
+        }
+      )
+    })
+  }
+
+  protected async update(): Promise<boolean> {
+    for (const symbol of this.symbols) {
+      await this.fetchLatestTrades(symbol)
+        .then((trades) => {
+          if (!trades.length) {
+            return
+          }
+
+          this.setTrades(symbol, trades)
+          this.setPrice(symbol, trades[trades.length - 1].price)
+        })
+        .catch(errorHandler)
+    }
+
+    return true
+  }
+}
