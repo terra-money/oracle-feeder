b4a895ceb0a3f15f5206d6a17b9b741654d7d059
bison-logan <52923580+ljk662@users.noreply.github.com>
2020-12-21 06:05:29 -0500

    Environment Variable Support for UpdateKey Program (#10)
    feat: environment variable support for UpdateKey

--- feeder/src/updateKey.ts
+++ feeder/src/updateKey.ts
@@ -2,22 +2,29 @@ import * as keystore from './keystore'
 import * as promptly from 'promptly'
 
 export async function updateKey(filePath: string): Promise<void> {
-  const password = await promptly.password(`Enter a passphrase to encrypt your key to disk:`, {
-    replace: `*`,
-  })
-  const confirm = await promptly.password(`Repeat the passphrase:`, { replace: `*` })
+  let password = process.env['PASSPHRASE'] || ''
+  let mnemonic = process.env['MNEMONIC'] || ''
+
+  if (password === '') {
+    password = await promptly.password(`Enter a passphrase to encrypt your key to disk:`, {
+      replace: `*`,
+    })
+    const confirm = await promptly.password(`Repeat the passphrase:`, { replace: `*` })
+
+    if (password !== confirm) {
+      console.error(`ERROR: passphrases don't matchPassword confirm failed`)
+      return
+    }
+  }
 
   if (password.length < 8) {
     console.error(`ERROR: password must be at least 8 characters`)
     return
   }
 
-  if (password !== confirm) {
-    console.error(`ERROR: passphrases don't matchPassword confirm failed`)
-    return
+  if (mnemonic === '') {
+    mnemonic = await promptly.prompt(`Enter your bip39 mnemonic: `)
   }
-
-  const mnemonic = await promptly.prompt(`Enter your bip39 mnemonic: `)
 
   if (mnemonic.trim().split(` `).length !== 24) {
     console.error(`Error: Mnemonic is not valid.`)
