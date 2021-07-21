d11e21415dc914bee7a1f20270155b7f2d8afc23
Yun Yeo <sw.yunsuk@gmail.com>
2021-07-21 17:06:38 +0900

    remove gateio support

--- price-server/src/provider/crypto/LunaProvider.ts
+++ price-server/src/provider/crypto/LunaProvider.ts
@@ -1,18 +1,17 @@
 import * as config from 'config'
 import { Provider, ProviderOptions } from 'provider/base'
-import { Binance, Bithumb, Coinone, Huobi, GateIO, Kucoin } from './quoter'
+import { Binance, Bithumb, Coinone, Huobi, Kucoin } from './quoter'
 
 class LunaProvider extends Provider {
   constructor(options: ProviderOptions) {
     super(options)
 
-    const { bithumb, coinone, huobi, binance, gateio, kucoin } = config.lunaProvider
+    const { bithumb, coinone, huobi, binance, kucoin } = config.lunaProvider
 
     bithumb && this.quoters.push(new Bithumb(bithumb))
     coinone && this.quoters.push(new Coinone(coinone))
     huobi && this.quoters.push(new Huobi(huobi))
     binance && this.quoters.push(new Binance(binance))
-    gateio && this.quoters.push(new GateIO(gateio))
     kucoin && this.quoters.push(new Kucoin(kucoin))
   }
 }
