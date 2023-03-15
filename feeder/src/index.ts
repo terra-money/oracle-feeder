96d1c0c57f91b7a0f793a1249231d30cb67c8b06
emidev98 <emilian@terra.money>
2023-03-15 12:17:23 +0200

    feat: refact arguments and add missing ones

--- feeder/src/index.ts
+++ feeder/src/index.ts
@@ -2,86 +2,108 @@ import { ArgumentParser } from 'argparse'
 import { vote } from './vote'
 import { addKey } from './addKey'
 import * as packageInfo from '../package.json'
+import * as promptly from 'promptly'
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
 function registerCommands(parser: ArgumentParser): void {
   const subparsers = parser.addSubparsers({
-    title: `commands`,
-    dest: `subparser_name`,
-    description: `Available commands`,
+    title: 'commands',
+    dest: 'subparser_name',
+    description: 'Available commands',
   })
 
   // Voting command
-  const voteCommand = subparsers.addParser(`vote`, {
+  const voteCommand = subparsers.addParser('vote', {
     addHelp: true,
-    description: `Fetch price and broadcast oracle messages`,
+    description: 'Fetch price and broadcast oracle messages',
   })
 
   voteCommand.addArgument(['-l', '--lcd-url'], {
+    help: 'lcd url address (default: process.env.LCD_URL or ["http://localhost:1317"])',
     action: 'append',
-    help: 'lcd address',
     dest: 'lcdUrl',
+    defaultValue: (process.env.LCD_URL && process.env.LCD_URL.split(',')) || ['http://localhost:1317'],
     required: false,
   })
 
-  voteCommand.addArgument([`-c`, `--chain-id`], {
-    action: `store`,
-    help: `chain ID`,
-    dest: `chainID`,
+  voteCommand.addArgument(['-c', '--chain-id'], {
+    help: 'chain id where the vote transactions must be submitted to (default: process.env.CHAIN_ID or "candle-1")',
+    action: 'store',
+    dest: 'chainID',
+    defaultValue: process.env.CHAIN_ID || 'candle-1',
     required: false,
   })
 
-  voteCommand.addArgument([`--validators`], {
-    action: `append`,
-    help: `validators address (e.g. terravaloper1...), can have multiple`,
+  voteCommand.addArgument(['--validators'], {
+    help: 'validators address(es) (e.g. terravaloper1...)',
+    action: 'append',
+    required: false,
+    defaultValue: process.env.VALIDATORS && process.env.VALIDATORS.split(','),
+  })
+
+  voteCommand.addArgument(['-d', '--data-source-url'], {
+    help: 'Append price(s) data source (default: process.env.DATA_SOURCE_URL or ["http://localhost:8532/latest"])',
+    action: 'append',
+    dest: 'dataSourceUrl',
+    defaultValue: (process.env.DATA_SOURCE_URL && process.env.DATA_SOURCE_URL.split(',')) || [
+      'http://localhost:8532/latest',
+    ],
     required: false,
   })
 
-  voteCommand.addArgument([`-d`, `--data-source-url`], {
-    action: `append`,
-    help: `Append price data source(It can handle multiple sources)`,
-    dest: `dataSourceUrl`,
-    required: false,
+  voteCommand.addArgument(['-p', '--password'], {
+    help: 'key store voter password (default: process.env.PASSWORD)',
+    action: 'store',
+    defaultValue: process.env.PASSWORD,
   })
 
-  voteCommand.addArgument([`-p`, `--password`], {
-    action: `store`,
-    help: `voter password`,
+  voteCommand.addArgument(['-k', '--key-path', '--keystore'], {
+    help: 'key store path where to save encrypted key (default: process.env.KEY_PATH or "voter.json")',
+    action: 'store',
+    dest: 'keyPath',
+    defaultValue: process.env.KEY_PATH || 'voter.json',
   })
 
-  voteCommand.addArgument([`-k`, `--key-path`, `--keystore`], {
-    action: `store`,
-    help: `key store path to save encrypted key`,
-    dest: `keyPath`,
-    required: false,
+  voteCommand.addArgument(['-n', '--key-name'], {
+    help: 'name to assing to the generated key inside the keystore (default: process.env.KEY_NAME or "voter")',
+    dest: 'keyName',
+    defaultValue: process.env.KEY_NAME || 'voter',
   })
 
-  voteCommand.addArgument([`-n`, `--key-name`], {
-    help: `name assigned to the generated key`,
-    dest: `keyName`,
-    defaultValue: `voter`,
+  voteCommand.addArgument(['-a', '--address-prefix'], {
+    help: 'prefix for the addresses to be generated (default: process.env.ADDR_PREFIX or "candle")',
+    dest: 'prefix',
+    defaultValue: process.env.ADDR_PREFIX || 'candle',
+  })
+
+  voteCommand.addArgument(['-v', '--voter'], {
+    help: 'addresses of the wallet to vote on behalf (default: process.env.VOTER or default stored address from keystore)',
+    dest: 'voter',
+    defaultValue: process.env.VOTER,
   })
 
   // Updating Key command
-  const keyCommand = subparsers.addParser(`add-key`, { addHelp: true })
-
-  keyCommand.addArgument([`-n`, `--key-name`], {
-    help: `name to assigns to the generated key`,
-    dest: `keyName`,
-    defaultValue: `voter`,
+  const addKeyCommand = subparsers.addParser('add-key', { addHelp: true })
+  addKeyCommand.addArgument(['-n', '--key-name'], {
+    help: 'name to assing to the generated key inside the keystore (default: process.env.KEY_NAME or "voter")',
+    dest: 'keyName',
+    defaultValue: process.env.KEY_NAME || 'voter',
   })
-
-  keyCommand.addArgument([`-t`, `--coin-type`], {
-    help: `coin type used to derive the public address (https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#path-levels)`,
-    dest: `coinType`,
-    defaultValue: `118`,
+  addKeyCommand.addArgument(['-t', '--coin-type'], {
+    help: 'coin type used to derive the public address (default: process.env.COIN_TYPE or "118")',
+    dest: 'coinType',
+    defaultValue: process.env.COIN_TYPE || '118',
   })
-
-  keyCommand.addArgument([`-k`, `--key-path`], {
-    help: `key store path to save encrypted key`,
-    dest: `keyPath`,
-    defaultValue: `voter.json`,
+  addKeyCommand.addArgument(['-k', '--key-path'], {
+    help: 'key store path where to save encrypted key (default: process.env.KEY_PATH or "voter.json")',
+    dest: 'keyPath',
+    defaultValue: process.env.KEY_PATH || 'voter.json',
+  })
+  addKeyCommand.addArgument(['-a', '--address-prefix'], {
+    help: 'prefix for the addresses to be generated (default: process.env.ADDR_PREFIX or "candle")',
+    dest: 'prefix',
+    defaultValue: process.env.ADDR_PREFIX || 'candle',
   })
 }
 
@@ -89,41 +111,19 @@ async function main(): Promise<void> {
   const parser = new ArgumentParser({
     version: packageInfo.version,
     addHelp: true,
-    description: `Terra oracle voter`,
+    description: 'Terra oracle voter',
   })
 
   registerCommands(parser)
   const args = parser.parseArgs()
 
-  args.prefix = args.prefix || process.env.ADDR_PREFIX
+  console.log(args)
 
-  if (args.subparser_name === `vote`) {
-    args.lcdUrl = args.lcdUrl || (process.env.LCD_URL && process.env.LCD_URL.split(',')) || []
-
-    args.dataSourceUrl =
-      args.dataSourceUrl || (process.env.DATA_SOURCE_URL && process.env.DATA_SOURCE_URL.split(',')) || []
-    args.chainID = args.chainID || process.env.CHAIN_ID || 'candled-testnet-1'
-    if (args.lcdUrl?.length === 0 || args.dataSourceUrl?.length === 0 || args.chainID === '') {
-      console.error('Missing --lcd, --chain-id or --data-source-url')
-      return
-    }
-
-    args.keyPath = args.keyPath || process.env.KEY_PATH || 'voter.json'
-    args.password = args.password || process.env.PASSWORD || ''
-    if (args.keyPath === '' || args.password === '') {
-      console.error('Missing either --key-path or --password')
-      return
-    }
-
-    // validators is skippable and default value will be extracted from the key
-    args.validators = args.validators || (process.env.VALIDATORS && process.env.VALIDATORS.split(','))
-
-    args.keyName = process.env.KEY_NAME ? process.env.KEY_NAME : args.keyName
+  if (args.subparser_name === 'vote') {
+    args.password = args.password || (await promptly.password(`Enter a passphrase:`, { replace: `*` }))
 
     await vote(args)
-  } else if (args.subparser_name === `add-key`) {
-    await addKey(args.keyPath, args.coinType, args.keyName, args.prefix)
-  }
+  } else if (args.subparser_name === 'add-key') await addKey(args)
 }
 
 main().catch((e) => {
