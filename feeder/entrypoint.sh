f63b0782b0009bcf25bd730a32a152a8aa143599
Till Ziegler <tz@schoeneweide.tk>
2023-08-31 07:19:28 +0200

    fix: feeder entrypoint.sh querying wrong oracle endpoint (#21)

--- feeder/entrypoint.sh
+++ feeder/entrypoint.sh
@@ -5,7 +5,7 @@ npm start add-key
 # check if voter has been bound to validator
 voter_addr=$(jq -r '.[0].address' voter.json)
 lcd=$(echo $ORACLE_FEEDER_LCD_ADDRESS | awk -F"," '{print $1}')
-feeder=$(curl "$lcd/oracle/voters/$ORACLE_FEEDER_VALIDATORS/feeder" | jq -r '.result')
+feeder=$(curl "$lcd/terra/oracle/v1beta1/validators/$ORACLE_FEEDER_VALIDATORS/feeder" | jq -r '.feeder_addr')
 
 if [ $voter_addr != $feeder ]; then
     echo "WRONG FEEDER. REGISTER IT THROUGH: terrad tx oracle set-feeder $voter_addr --from=<validator>"
